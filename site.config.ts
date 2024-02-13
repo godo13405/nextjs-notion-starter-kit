1777a3bc9632de90e6d0c6796f7dafe4fb5263ff
Gonçalo Andrade <goncaloandradedovale@gmail.com>
2024-02-13 11:53:42 +0000

    init

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Goncalo-Andrade-e7444b32839745d2b6199b5f97ae78e9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Goncalo Andrade | Design',
+  domain: 'next.goncaloandrade.com',
+  author: 'Goncalo Andrade',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'transitive_bs',
   github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  linkedin: 'goncaloandrade',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
