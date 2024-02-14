dcd95441e6a5ecf3c39c48f9832d801e5463a141
Goncalo Andrade <goncaloandradedovale@gmail.com>
2024-02-14 15:18:24 +0000

    done?

--- site.config.ts
+++ site.config.ts
@@ -1,4 +1,5 @@
-import { siteConfig } from './lib/site-config'
+import { siteConfig } from './lib/site-config';
+
 
 export default siteConfig({
   // the site's root Notion page (required)
@@ -9,25 +10,26 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Goncalo Andrade | Design',
+  name: 'Goncalo Stratford Andrade | Design',
   domain: 'portfolio.goncaloandrade.com',
-  author: 'Goncalo Andrade',
+  author: 'Goncalo Stratford Andrade',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Goncalo Andrade\'s portfolio and personal site',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
   // github: 'transitive-bullshit',
-  linkedin: 'goncaloandrade',
+  // linkedin: 'goncaloandrade',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  // defaultPageIcon: "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F9fc14744-02f6-4234-874a-557dc12379ff%2Fezgif.com-resize.gif?table=block&id=e7444b32-8397-45d2-b619-9b5f97ae78e9&cache=v2",
-  defaultPageIcon: null,
+  defaultPageIcon:
+    'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F9fc14744-02f6-4234-874a-557dc12379ff%2Fezgif.com-resize.gif?table=block&id=e7444b32-8397-45d2-b619-9b5f97ae78e9&cache=v2',
+  // defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0,
 
@@ -51,16 +53,18 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Work',
+      pageId:
+        '2e1f47b4f5954b07a15f2bab10225cee?v=7cb7c1ef65d64933bf3c15b75a4e0bc4'
+    },
+    {
+      title: 'Testimonials',
+      pageId:
+        '0dab09b9f9064f5fb259af6d1f56fafd?v=acdc7b20cfde4ff697a97ed48e378421&pvs=4'
+    }
+  ]
 })
