d48a35d53661acb217c5226225d08027a2cfd865
Goncalo Andrade <goncaloandradedovale@gmail.com>
2024-03-06 14:13:27 +0000

    .env file

--- site.config.ts
+++ site.config.ts
@@ -3,19 +3,19 @@ import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Goncalo-Andrade-e7444b32839745d2b6199b5f97ae78e9',
+  rootNotionPageId: process.env.ROOTID,
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Goncalo Stratford Andrade | Design',
-  domain: 'portfolio.goncaloandrade.com',
-  author: 'Goncalo Stratford Andrade',
+  name: process.env.SITENAME || '',
+  domain: process.env.SITEDOMAIN || '',
+  author: process.env.SITEAUTHOR || '',
 
   // open graph metadata (optional)
-  description: "Goncalo Andrade's portfolio and personal site",
+  description: process.env.SITEDESCRIPTION || '',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
@@ -27,9 +27,8 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon:
-    'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F9fc14744-02f6-4234-874a-557dc12379ff%2Fezgif.com-resize.gif?table=block&id=e7444b32-8397-45d2-b619-9b5f97ae78e9&cache=v2',
-  // defaultPageIcon: null,
+  // defaultPageIcon: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F9fc14744-02f6-4234-874a-557dc12379ff%2Fezgif.com-resize.gif?table=block&id=e7444b32-8397-45d2-b619-9b5f97ae78e9&cache=v2',
+  defaultPageIcon: null,
   defaultPageCover: null,
   defaultPageCoverPosition: 0,
 
@@ -46,25 +45,14 @@ export default siteConfig({
   // example:
   //
   // pageUrlOverrides: null,
-  pageUrlOverrides: {
-    '/work':
-      '2e1f47b4f5954b07a15f2bab10225cee',
-    '/testimonials': '0dab09b9f9064f5fb259af6d1f56fafd'
-  },
+  pageUrlOverrides: process.env.PAGEURLOVERRIDES
+    ? JSON.parse(process.env.PAGEURLOVERRIDES)
+    : null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
   // navigationStyle: 'default'
-  navigationStyle: 'custom',
-  navigationLinks: [
-    {
-      title: 'Work',
-      pageId:
-        '2e1f47b4f5954b07a15f2bab10225cee'
-    },
-    {
-      title: 'Testimonials',
-      pageId: '0dab09b9f9064f5fb259af6d1f56fafd'
-    }
-  ]
+  navigationStyle: process.env.NAVIGATIONLINKS ? 'custom' : 'default',
+  navigationLinks:
+    process.env.NAVIGATIONLINKS ? JSON.parse(process.env.NAVIGATIONLINKS) : null
 })
