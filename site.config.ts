e0f0da264d806f4935f74326bc3ce74df56fa289
Goncalo Andrade <goncaloandradedovale@gmail.com>
2024-05-31 11:09:40 +0100

    next env vars

--- site.config.ts
+++ site.config.ts
@@ -3,19 +3,20 @@ import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Goncalo-Andrade-e7444b32839745d2b6199b5f97ae78e9',
+  // rootNotionPageId: 'Goncalo-Andrade-e7444b32839745d2b6199b5f97ae78e9',
+  rootNotionPageId: process.env.NEXT_PUBLIC_ROOTID,
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Goncalo Stratford Andrade | Design',
-  domain: 'portfolio.goncaloandrade.com',
-  author: 'Goncalo Stratford Andrade',
+  name: process.env.NEXT_PUBLIC_SITENAME,
+  domain: process.env.NEXT_PUBLIC_SITEDOMAIN,
+  author: process.env.NEXT_PUBLIC_SITEAUTHOR,
 
   // open graph metadata (optional)
-  description: "Goncalo Andrade's portfolio and personal site",
+  description: process.env.NEXT_PUBLIC_SITEDESCRIPTION,
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
@@ -46,28 +47,11 @@ export default siteConfig({
   // example:
   //
   // pageUrlOverrides: null,
-  pageUrlOverrides: {
-    '/work': '2e1f47b4f5954b07a15f2bab10225cee',
-    '/testimonials': '0dab09b9f9064f5fb259af6d1f56fafd',
-    '/resume': 'ebd2ad71c50e47a991258e378b734975'
-  },
+  pageUrlOverrides: JSON.parse(process.env.NEXT_PUBLIC_PAGEURLOVERRIDES),
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
   // navigationStyle: 'default'
   navigationStyle: 'custom',
-  navigationLinks: [
-    {
-      title: 'Resume',
-      pageId: 'ebd2ad71c50e47a991258e378b734975'
-    },
-    {
-      title: 'Work',
-      pageId: '2e1f47b4f5954b07a15f2bab10225cee'
-    },
-    {
-      title: 'Testimonials',
-      pageId: '0dab09b9f9064f5fb259af6d1f56fafd'
-    }
-  ]
+  navigationLinks: JSON.parse(process.env.NEXT_PUBLIC_NAVIGATIONLINKS)
 })
