b78cbe5519c537c7e0426f29eee83774158d1aa5
Goncalo Andrade <goncaloandradedovale@gmail.com>
2024-07-02 14:44:56 +0100

    fixed horizontal scrolling, added ellipsis to sitename

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -24,10 +24,8 @@ export const PageHead: React.FC<
     <Head>
       <meta charSet='utf-8' />
       <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
-      <meta
-        name='viewport'
-        content='width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover'
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
 
       <meta name='apple-mobile-web-app-capable' content='yes' />
       <meta
